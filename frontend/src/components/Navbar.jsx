485f873910954840245b715154e21f1724d91e8d
Sonali Manjula <sonalijayakodi2002@gmail.com>
2025-11-21 15:31:38 +0530

    2 changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,54 @@
-import { Button, Container, Flex, HStack } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container maxW={"1140px"} px={4}>
+    <Container
+      maxW="1140px"
+      px={4}
+      bg={useColorModeValue("gray.100", "gray.900")}
+    >
       <Flex
         h={16}
-        alignitems={"centter"}
-        justifycontent={"space-between"}
-        flexDir={{
-          base: "column",
-          sm: "row",
-        }}
+        alignItems="center"
+        justifyContent="space-between"
+        flexDir="row" // Always row
       >
+        {/* Left: Title */}
         <Text
-          bgGradient={"linear(to-r, teal.500, green.500)"}
-          textTransform={"uppercase"}
-          bgClip={"text"}
-          fontSize={"4xl"}
-          fontWeight={"bold"}
+          bgGradient="linear(to-r, teal.500, green.500)"
+          textTransform="uppercase"
+          bgClip="text"
+          fontSize="4xl"
+          fontWeight="bold"
         >
-          <Link to={"/"}>Product Strore</Link>
+          <Link to="/">Product Store</Link>
         </Text>
 
-        <HStack spacing={2} alignitems={"center"}>
-          <Link to={"/create"}>
+        {/* Right: Buttons */}
+        <HStack spacing={2}>
+          <Link to="/create">
             <Button>
-              <PlusSquareIcon />
+              <CiSquarePlus fontSize={20} />
             </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <LuSun /> : <IoMoon size="20" />}
+          </Button>
         </HStack>
       </Flex>
     </Container>
